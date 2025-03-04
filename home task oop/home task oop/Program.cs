2cb6fdbd0159ac22991aea5978aa1e3e1ee9a47e
zainabxhk <zainabxhk4@gmail.com>
2025-03-04 20:00:24 +0500

    heyy this is me

---
+++ home task oop/home task oop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.title = "janat k pate";
+            book1.Author = "namra ahmad";
+            book1.ISBN ="45676";
+            book1.Price = 5000;
+
+            book1.DisplayDatail();
+            book1.ApplyDiscount(10);
+        }
+    }
+}
